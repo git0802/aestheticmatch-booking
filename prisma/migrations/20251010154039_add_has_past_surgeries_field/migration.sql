ed420b420db87d8748a486ec059af227ce6366ef
git0802 <t2k20802@gmail.com>
2025-10-10 12:09:40 -0400

    feat: add migration to drop has_past_surgeries column from patients table with warning notice

---
+++ prisma/migrations/20251010154039_add_has_past_surgeries_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "patients" ADD COLUMN     "has_past_surgeries" BOOLEAN DEFAULT false;
---
+++ prisma/migrations/20251010160254_remove_has_past_surgeries_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `has_past_surgeries` on the `patients` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "patients" DROP COLUMN "has_past_surgeries";
